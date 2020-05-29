6bdd4c1b7ec4eb75b8798585d4c35be80d69386b
Bob Qi <baobin.qi@fmr.com>
2020-05-29 21:11:01 +0800

    add structopt

--- README.md
+++ README.md
@@ -4,6 +4,22 @@ Automatically move mouse to avoid screen saver
 
 Currently the tools will move the mouse location by 5px offset per 60 seconds.
 
+``` sh
+mouse mover 0.1.1
+Mouse mover usage.
+
+USAGE:
+    mouse-mover [OPTIONS]
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+OPTIONS:
+    -d, --distance <distance>    set the distance delta when we want to move [default: 5]
+    -i, --interval <interval>    set the time interval in seconds, how often we run [default: 60]
+```
+
 # install
 
 * Just download latest release from https://github.com/QiBaobin/mouse-auto-mover/releases
--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,36 @@ use enigo::*;
 use std::thread;
 use std::time::Duration;
 
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "mouse mover", about = "Mouse mover usage.")]
+struct Opt {
+    /// set the distance delta when we want to move
+    #[structopt(short, long, default_value = "5")]
+    distance: usize,
+
+    /// set the time interval in seconds, how often we run
+    #[structopt(short, long, default_value = "60")]
+    interval: u64,
+}
+
 pub fn main() {
+    let opt = Opt::from_args();
+
     let mut enigo = Enigo::new();
     let size = Enigo::main_display_size();
 
+    let edge = 0..(opt.distance as i32) + 1;
+    let offset = -(opt.distance as i32);
     loop {
-        thread::sleep(Duration::from_secs(60));
+        thread::sleep(Duration::from_secs(opt.interval));
 
-        match Enigo::mouse_location() {
-            (0, 0) => enigo.mouse_move_to(size.0 as i32, size.1 as i32),
-            _ => enigo.mouse_move_relative(-5, -5),
+        let position = Enigo::mouse_location();
+        if edge.contains(&position.0) || edge.contains(&position.1) {
+            enigo.mouse_move_to(size.0 as i32, size.1 as i32);
+        } else {
+            enigo.mouse_move_relative(offset, offset);
         }
     }
 }
