f16303cb41f077946cbdb6780f78923056d64a41
Bob Qi <baobin.qi@fmr.com>
2020-06-05 14:36:12 +0800

    add verbose message print out

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use enigo::*;
 use std::thread;
 use std::time::Duration;
 
+use std::io::{self, Result, Write};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -14,9 +15,12 @@ struct Opt {
     /// set the time interval in seconds, how often we run
     #[structopt(short, long, default_value = "60")]
     interval: u64,
+
+    #[structopt(short, long)]
+    verbose: bool,
 }
 
-pub fn main() {
+pub fn main() -> Result<()> {
     let opt = Opt::from_args();
 
     let mut enigo = Enigo::new();
@@ -25,13 +29,35 @@ pub fn main() {
     let edge = 0..(opt.distance as i32) + 1;
     let offset = -(opt.distance as i32);
     loop {
+        opt.log_fmt(format_args!("Sleep for {}", opt.interval))?;
         thread::sleep(Duration::from_secs(opt.interval));
 
         let position = Enigo::mouse_location();
+        opt.log_fmt(format_args!(
+            "Wake up at mouse cursor position at {} {}",
+            position.0, position.1
+        ))?;
+
         if edge.contains(&position.0) || edge.contains(&position.1) {
             enigo.mouse_move_to(size.0 as i32, size.1 as i32);
         } else {
             enigo.mouse_move_relative(offset, offset);
         }
+
+        let position = Enigo::mouse_location();
+        opt.log_fmt(format_args!(
+            "Tried to move and now at {} {}",
+            position.0, position.1
+        ))?;
+    }
+}
+
+impl Opt {
+    fn log_fmt(&self, fmt: std::fmt::Arguments) -> Result<()> {
+        if self.verbose {
+            io::stdout().write_fmt(fmt)?;
+            io::stdout().write_all(b"\n")?;
+        }
+        Ok(())
     }
 }
